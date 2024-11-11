7e15aa73a9e16b22816b62160d4151404d7454d6
mazer <mazer0701@gmail.com>
2024-11-12 01:36:08 +0800

    add interaction module

---
+++ src/interaction.py
@@ -0,0 +1,69 @@
+"""A module for defining use cases and output interfaces to interact with clients."""
+
+from abc import ABC, abstractmethod
+
+
+class OutputHandler(ABC):
+    """
+    An interface for handling output operations.
+
+    This abstract class allows different output mechanisms (e.g., console, UI, network)
+    to be injected into the system. This helps decouple the core application logic from
+    the specifics of how data is presented or transmitted.
+    """
+
+    @abstractmethod
+    def output(self, data: dict) -> None:
+        """Handles the task of outputting data, such as presenting it to the user or storing it."""
+
+    @abstractmethod
+    def error_output(self, code: int, message: str) -> None:
+        """Handles the task of error message, such as raise an error or log it."""
+
+
+class ConsoleOutput(OutputHandler):
+    """An output handler that displays data to the console."""
+
+    def output(self, data: dict) -> None:
+        """Outputs the data to the console."""
+        print(data)
+
+    def error_output(self, code: int, message: str) -> None:
+        """Outputs the error message to the console."""
+        print(f"Error code {code}: {message}")
+
+
+class MemoNest(ABC):
+    """
+    A use case class that manages MemoNest-related operations.
+
+    The MemoNest class encapsulates the business logic for memo operations. It is
+    designed to be extended with additional functionality (e.g., updating or deleting
+    memos) while delegating output responsibilities to the OutputHandler. This ensures
+    that MemoNest is decoupled from any specific output implementation and adheres
+    to the Dependency Inversion Principle.
+    """
+
+    def __init__(self) -> None:
+        self.output_handler = None
+
+    def set_output(self, output: OutputHandler) -> None:
+        """
+        Sets the output handler to be used for outputting data.
+
+        The output handler is responsible for presenting the result or data
+        to the client (UI, database, etc.), following the Dependency Inversion Principle.
+        """
+        self.output_handler = output
+
+    def output(self, data: dict) -> None:
+        """Delegates the task of outputting data to the output handler."""
+
+        if self.output_handler is not None:
+            self.output_handler.output(data)
+
+    def error(self, code: int, data: str) -> None:
+        """Delegates the task of outputting error data to the output handler."""
+
+        if self.output_handler is not None:
+            self.output_handler.error_output(code, data)
---
+++ tests/unit/test_interaction.py
@@ -0,0 +1,62 @@
+import unittest
+from io import StringIO
+from unittest.mock import Mock, patch
+
+from src.interaction import ConsoleOutput, MemoNest, OutputHandler
+
+
+class TestMemoNest(unittest.TestCase):
+    def setUp(self):
+        self.memo_nest = MemoNest()
+        self.mock_output_handler = Mock(spec=OutputHandler)
+        self.memo_nest.set_output(self.mock_output_handler)
+
+    def test_set_output(self):
+        self.memo_nest.set_output(self.mock_output_handler)
+        self.assertEqual(self.memo_nest.output_handler, self.mock_output_handler)
+
+    def test_output(self):
+        test_data = {"title": "Test Memo", "description": "This is a test memo."}
+        self.memo_nest.output(test_data)
+        self.mock_output_handler.output.assert_called_once_with(test_data)
+
+        self.memo_nest.set_output(None)
+        self.memo_nest.output(test_data)  # not raising an error
+
+    def test_error(self):
+        error_code = 404
+        error_message = "Not Found"
+        self.memo_nest.error(error_code, error_message)
+        self.mock_output_handler.error_output.assert_called_once_with(
+            error_code, error_message
+        )
+
+        self.memo_nest.set_output(None)
+        self.memo_nest.error(error_code, error_message)  # not raising an error
+
+
+class TestConsoleOutput(unittest.TestCase):
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_output(self, mock_stdout):
+        console_output = ConsoleOutput()
+        test_data = {"title": "Test Memo", "description": "This is a test memo."}
+
+        console_output.output(test_data)
+
+        output = mock_stdout.getvalue().strip()
+        self.assertEqual(output, str(test_data))
+
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_error_output(self, mock_stdout):
+        console_output = ConsoleOutput()
+        error_code = 404
+        error_message = "Not Found"
+
+        console_output.error_output(error_code, error_message)
+
+        output = mock_stdout.getvalue().strip()
+        self.assertEqual(output, f"Error code {error_code}: {error_message}")
+
+
+if __name__ == "__main__":
+    unittest.main()
