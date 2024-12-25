76a3a956371925dad7b1c48fa73ca05961af7877
mazer <mazer0701@gmail.com>
2024-12-25 23:22:36 +0800

    add fail-under & set -o errexit for CI

--- coding_style_check.sh
+++ coding_style_check.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+set -o errexit
+
 echo "main code coding style check:"
-pylint --recursive=y --rcfile=./src/.pylintrc --fail-under=10.0 src
+pylint --recursive=y --rcfile=./src/.pylintrc --fail-under 10 src
 black --check --diff src
 isort --check --diff --profile black src
 
 echo "test code coding style check:"
-pylint --recursive=y --rcfile=./tests/.pylintrc --fail-under=10.0 tests
+pylint --recursive=y --rcfile=./tests/.pylintrc --fail-under 10 tests
 black --check --diff tests
 isort --check --diff --profile black tests
 
 echo "example code coding style check:"
-pylint --recursive=y --rcfile=./example/.pylintrc --fail-under=10.0 example
+pylint --recursive=y --rcfile=./example/.pylintrc --fail-under 10 example
 black --check --diff example
 isort --check --diff --profile black example
--- testing.sh
+++ testing.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+set -o errexit
+
 echo "unit tests:"
 python -m unittest
